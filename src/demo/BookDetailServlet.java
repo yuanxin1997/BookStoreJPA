965ff17af952e16e761fc79e7fcd1565ae461d92
yuanxin <154405A@mymail.nyp.edu.sg>
2016-12-08 15:57:49 +0800

    Initial commit

---
+++ src/demo/BookDetailServlet.java
@@ -0,0 +1,34 @@
+package demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.*;
+/**
+ * Created by Owner on 12/8/2016.
+ */
+@WebServlet(name = "BookDetailServlet", urlPatterns = "/bookdetails")
+public class BookDetailServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String bookId = request.getParameter("bookId");
+            if (bookId != null) {
+                BookDAO db = new BookDAO();
+                BooksEntity bd = db.getBookDetails(bookId);
+                // store bookdetails object in request scope with attribut name "book"
+                // bookdetails.jsp will retrieve this stored object later to display the content
+                request.setAttribute("book", bd);
+                // forward the request to bookdetails.jsp, there must be a "/" before the web resource reference
+                getServletContext().getRequestDispatcher("/bookdetails.jsp").forward(request,response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+}
